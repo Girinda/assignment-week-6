27381b0014c7d528f30050c5d18fd1f7bb8061ee
Girinda <girindade@gmail.com>
2025-06-12 18:34:45 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-1
+
+
+let rootpath https:///mysite.itvarsity.org/apii/contactbook/";
+let apikey= "cheackApikey"();
+
+function checkApikey() {
+        if (:localstorage.getitem("Apikey")){
+        window.open("enter-Api-Key.html","self");
+} 
+        return localstorage.getitem("ApiKey");
+}
